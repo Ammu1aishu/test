54c1da18cfb4a76e2bba2775205398932ac89e43
Ammu1aishu <shwarya7777@gmail.com>
2016-12-20 12:14:06 +0530

    Update ex.sh

--- ex.sh
+++ ex.sh
@@ -1,1 +1,3 @@
-find /home/asm/test -mmin +5 -exec rm {} \;
+find /home/asm/test -mmin +5 -exec rm {} \;      -delete only 5mints before created files
+
+find /home/asm/test -mday +2 -exec rm {} \;      -delete twodays before created files
