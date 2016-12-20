0d0d987ddbddcff298308c3f7d5f237096eec031
ASM <asm@asm-Vostro-260s.(none)>
2016-12-20 18:01:05 +0530

    done

---
+++ newcalc.py
@@ -0,0 +1,22 @@
+try:
+	number_1 = int(input('Please enter the first number: '))
+	number_2 = int(input('Please enter the second number: '))
+	#def calculate():
+	operation = raw_input("Please type in the math operation you would like to complete:")
+	if operation == '+':
+        	print('{} + {} = '.format(number_1, number_2))
+        	print(number_1 + number_2)
+
+	elif operation == '-':
+        	print('{} - {} = '.format(number_1, number_2))
+        	print(number_1 - number_2)
+
+	elif operation == '*':
+        	print('{} * {} = '.format(number_1, number_2))
+        	print(number_1 * number_2)
+
+	elif operation == '/':
+        	print('{} / {} = '.format(number_1, number_2))
+        	print(number_1 / number_2)
+except ArithmeticError as err:
+	print("you are entering the invalid operator")
